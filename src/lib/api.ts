30146fb9417445d9a9811e507968ba8e98b3af6a
Brentph505 <abrazaldo1989@gmail.com>
2025-01-10 11:19:48 +0800

    Update api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: 'https://aniwatch-api-amber.vercel.app/api/v2/hianime'
+    baseURL: 'https://corsproxy.io/?url=https://aniwatch-api-amber.vercel.app/api/v2/hianime'
 })
